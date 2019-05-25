78a366c45d710323eaac63f0bcf8ea313ab3418b
Divo Kaplan <divokaplan@gmail.com>
2019-05-25 03:20:36 +0300

    Remove hardcoded packages and refactor selection. Package selection is now done via an online list. This replaces the old hardcoded list used for testing. A config file has been added.

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
-import setuptools
-import random
+import json
 import pathlib
+import random
 import tempfile
+import urllib.request
 
-POSSIBLE_DEPS = [
-    'requests',
-    'colorama',
-    'loguru',
-    'pygments',
-    'pytest',
-    'freezegun',
-]
+import setuptools
 
-dep = random.choice(POSSIBLE_DEPS)
+config_path = pathlib.Path('config.json')
+config = json.load(config_path.open('r'))
 
-dep_lock_path = pathlib.Path(f'{tempfile.gettempdir()}/._pkg_dep.lock')
+# TODO: give credit. thank you!
+response = urllib.request.urlopen(config['top_pypi_packages_link'])
+possible_packages = json.loads(response.read())['rows'][:config['top_x_packages']]
+chosen_dep = random.choice([package['project'] for package in possible_packages])
+
+dep_lock_path = pathlib.Path(tempfile.gettempdir()).joinpath(config['lockfile_name'])
 
 if dep_lock_path.exists():
-    dep = dep_lock_path.read_text().strip()
+    chosen_dep = dep_lock_path.read_text().strip()
     dep_lock_path.unlink()
 else:
-    dep_lock_path.write_text(dep)
+    dep_lock_path.write_text(chosen_dep)
 
 init_py_path = pathlib.Path('mystery')
 init_py_path.mkdir(exist_ok=True)
@@ -28,9 +28,11 @@ init_py_path = init_py_path / '__init__.py'
 init_py_path.write_text(
     f'''
 def _import_guard():
-    import {dep}
+    # I'd like to get rid of all the imports inside the function's scope.
+    # Less headache during cleanup.
+    import {chosen_dep}
     import sys
-    sys.modules['mystery'] = {dep}
+    sys.modules['mystery'] = {chosen_dep}
 _import_guard()
 del _import_guard
 '''
@@ -40,5 +42,5 @@ setuptools.setup(
     name='mystery',
     description='It is a riddle, wrapped in a mystery, inside an enigma.',
     packages=setuptools.find_packages(),
-    install_requires=[dep],
+    install_requires=[chosen_dep],
 )
