455c91aa82387df20a6a5e497d779aee7e4ac2bb
Divo Kaplan <divokaplan@gmail.com>
2019-05-25 23:43:29 +0300

    Restructure code with functions + offline support. Prettify code with functions. Add support for offline installation. Add offline json of top pypi packages. Adjust the config file accordingly.

--- setup.py
+++ setup.py
@@ -3,51 +3,91 @@ import pathlib
 import random
 import tempfile
 import urllib.request
+import typing
 
 import setuptools
 
 # Load the configuration file.
-config_path = pathlib.Path('config.json')
-config = json.load(config_path.open('r'))
+CONFIG_PATH = pathlib.Path('config.json')
+CONFIG = json.load(CONFIG_PATH.open('r'))
 
-# To keep the chosen dependency consistent in between setup.py runs, 'mystery' uses a temporary lockfile.
-dep_lock_path = pathlib.Path(tempfile.gettempdir()).joinpath(config['lockfile_name'])
-if dep_lock_path.exists():
-    # Use the locked package and unlink the file.
-    chosen_package = dep_lock_path.read_text().strip()
-    dep_lock_path.unlink()
-else:
-    # Get the top PyPI packages and use one of them.
-    response = urllib.request.urlopen(
-        config['top_pypi_packages_link']
-    )  # TODO: give credit. thank you!
-    possible_packages = json.loads(response.read())['rows'][: config['top_x_packages']]
-    chosen_package = random.choice(
-        [package['project'] for package in possible_packages]
+
+def get_package_list() -> typing.List[str]:
+    """
+    Get a list of possible packages.
+
+    :return: list of package names.
+    :rtype: typing.List[str]
+    """
+    try:
+        # Get the top PyPI packages and use one of them.
+        response = urllib.request.urlopen(
+            CONFIG['top_pypi_packages_link']
+        )  # TODO: give credit. thank you!
+        possible_packages_raw = response.read()
+    except urllib.request.URLError:
+        # Use the offline backup file.
+        with open(CONFIG['top_pypi_packages_offline_backup'], 'r') as backup_file:
+            possible_packages_raw = backup_file.read()
+    return json.loads(possible_packages_raw)['rows'][: CONFIG['top_x_packages']]
+
+
+def choose_mystery_package() -> str:
+    """
+    Choose the underlying mysterious package.
+
+    :return: mystery package name.
+    :rtype: str
+    """
+    # To keep the chosen dependency consistent in between setup.py runs, 'mystery' uses a temporary lockfile.
+    dep_lock_path = pathlib.Path(tempfile.gettempdir()).joinpath(
+        CONFIG['lockfile_name']
     )
-    dep_lock_path.write_text(chosen_package)  # Lock the chosen package of course.
+    if dep_lock_path.exists():
+        # Use the locked package and unlink the lockfile.
+        chosen_package = dep_lock_path.read_text().strip()
+        dep_lock_path.unlink()
+    else:
+        # Choose a package and create the lockfile.
+        possible_packages = get_package_list()
+        chosen_package = random.choice(
+            [package['project'] for package in possible_packages]
+        )
+        dep_lock_path.write_text(chosen_package)  # Lock the chosen package of course.
+    return chosen_package
 
 
-# Dynamically write the __init__.py file for the 'mystery' package using the chosen package.
-init_py_path = pathlib.Path('mystery')
-init_py_path.mkdir(exist_ok=True)
-init_py_path = init_py_path / '__init__.py'
-init_py_path.write_text(
-    f'''
+def write_init_py(package_name: str) -> None:
+    """
+    Dynamically write the __init__.py for the package using the chosen package.
+
+    :param chosen_package: mystery package name.
+    :type chosen_package: str
+    :rtype: None
+    """
+    init_py_path = pathlib.Path('mystery')
+    init_py_path.mkdir(exist_ok=True)
+    init_py_path = init_py_path / '__init__.py'
+    init_py_path.write_text(
+        f'''
 def _import_guard():
     # I'd like to get rid of all the imports inside the function's scope.
     # Less headache during cleanup.
-    import {chosen_package}
+    import {package_name}
     import sys
-    sys.modules['mystery'] = {chosen_package}
+    sys.modules['mystery'] = {package_name}
 _import_guard()
 del _import_guard
 '''
-)
+    )
+
+
+CHOSEN_PACKAGE = choose_mystery_package()
+write_init_py(CHOSEN_PACKAGE)
 
 setuptools.setup(
     name='mystery',
     description='It is a riddle, wrapped in a mystery, inside an enigma.',
     packages=setuptools.find_packages(),
-    install_requires=[chosen_package],
+    install_requires=[CHOSEN_PACKAGE],
 )
